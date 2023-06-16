659e04a929a6a02066b7186e574f6891c68b345c
JazzBonezz <107055803+JazzBonezz@users.noreply.github.com>
2023-06-16 21:43:54 +0500

    Under developing

---
+++ script.js
@@ -0,0 +1,8 @@
+// console.log("Z патриот")
+// alert("Ты патриот?")
+
+// const firstname = prompt('Введите имя!')
+// alert(firstname)
+
+const g = Math.PI
+console.log (Math.max(45, 53, 43, 23, 654, 756))
